3e1116dcd2c24c7bb930bf86624e0c18c1749bdc
Jose-Lema <jose.lema.2310@gmail.com>
2024-02-03 21:07:58 -0800

    Added examples to explain literals and objects

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ function Rectangle(name, width, height) {
 
 const rect1 = new Rectangle('Rectangle 1', 12, 13);
 console.log(rect1.area());
+
+// String Literal
+const strLit = 'Hello';
+const strObj = new String('Hello');
+
+console.log(strLit, typeof strLit);
+console.log(strObj, typeof strObj);
+
+// Boxing: Wrapping a primitive in an invisible object
+console.log(strLit.toUpperCase());
+console.log(strLit[0]);
+
+// Unboxing: starting with an object and turning it into a literal
+console.log(strObj.valueOf(), typeof strObj.valueOf());
+
+console.log(strLit.constructor);
+console.log(strObj.constructor);
+
+console.log(strLit instanceof String);
+console.log(strObj instanceof String);
